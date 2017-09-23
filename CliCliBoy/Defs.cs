638c3f9fd55a39c12ab7d0112d4124d56dee342e
toyota-m2k <toyota.mitsuki@gmail.com>
2017-09-23 12:41:23 +0900

    待ち時間の初期値を100-->1000msに変更。

--- CliCliBoy/Defs.cs
+++ CliCliBoy/Defs.cs
@@ -8,7 +8,7 @@ namespace CliCliBoy
 {
     public static class Defs
     {
-        public const int INITIAL_WAIT = 100;        // Target のWAITの初期値　（ｍｓ）
+        public const int INITIAL_WAIT = 1000;        // Target のWAITの初期値　（ｍｓ）
         public const int MINIMUM_WAIT = 50;        // Target のWAITの初期値　（ｍｓ）
     }
 }
